c272d7bd165252d56d05a2e475753bf57c9c2836
tesla369 <tesla369@gmail.com>
2018-02-20 14:52:01 +0100

    Working on objective version in progress...

--- snake.js
+++ snake.js
@@ -3,26 +3,42 @@
  */
 (function() {
 
-    var mapHeight = 10,
-        mapWidth = 10;
-    
-    var map = $("div#map");
-    for( var i = 0; i < mapHeight; i++ ) {
-        var row = $("<div class='row'></div>");
-        for( var j = 0; j < mapWidth; j++ ){
-            row.append($("<div class='field' data-x='"+i+"' data-y='"+j+"'></div>"));
+    function Map(map) {
+        this.map = map;
+
+        this.mapHeight = 10;
+        this.mapWidth = 10;
+
+        this.snakeBody = [];
+        this.snakeBody[0] = [4,4];
+        this.snakeBody[1] = [3,4];
+        this.snakeBody[2] = [2,4];
+
+        this.direction = "down";
+        this.oneDirection = "down";
+        this.time = 200;
+
+        for( var i = 0; i < this.mapHeight; i++ ) {
+            this.row = $("<div class='row'></div>");
+            for( var j = 0; j < this.mapWidth; j++ ){
+                this.row.append($("<div class='field' data-x='"+i+"' data-y='"+j+"'></div>"));
+            }
+            this.map.append(this.row);
         }
-        map.append(row);
+
+        this.snakeInit(this.snakeBody);
+
+        this.oneDirection = this.listener();
+
+        this.theGame(this.oneDirection);
+
     }
 
-    var direction = "down";
-    var oneDirection = "down";
-    var time = 200;
 
-    var snakeBody = [];
-    snakeBody[0] = [4,4];
-    snakeBody[1] = [3,4];
-    snakeBody[2] = [2,4];
+
+
+
+
     var snakeTail = [];
     var bugLocation = [7,8];
     var buggy = $("div.field[data-x='"+bugLocation[0]+"'][data-y='"+bugLocation[1]+"']");
@@ -32,107 +48,128 @@
         snakeTail[i-1]=snakeBody[i];
     }
 
-    document.addEventListener('keypress', function(event) {
+    Map.prototype.listener = function() {
+        document.addEventListener('keypress', function(event) {
 
-       var keyName = event.key;
-       switch(keyName) {
-           case 'ArrowRight':
-                   oneDirection = "right";
-               break;
-           case 'ArrowLeft':
-                   oneDirection = "left";
-               break;
-           case 'ArrowUp':
-                   oneDirection = "up";
-               break;
-           case 'ArrowDown':
-                   oneDirection = "down";
-               break;
-       }
-       console.log(keyName);
-    });
+            var keyName = event.key;
+            var oneDirection = this.oneDirection;
+            switch(keyName) {
+                case 'ArrowRight':
+                    oneDirection = "right";
+                    break;
+                case 'ArrowLeft':
+                    oneDirection = "left";
+                    break;
+                case 'ArrowUp':
+                    oneDirection = "up";
+                    break;
+                case 'ArrowDown':
+                    oneDirection = "down";
+                    break;
+            }
+            return oneDirection;
+        });
+    };
 
-    snakeBody.forEach(function(e) {
+    Map.prototype.snakeInit = function(snakeBody){
+        snakeBody.forEach(function(e) {
         var body = $("div[data-x='" + e[0] + "'][data-y='" + e[1] + "']");
         body.css("background-color", "black");
     });
+    };
 
-    var theGame = setInterval(function(){
-        switch(oneDirection) {
-            case 'down':
-                if(direction!=='up') {
-                    direction = 'down';}
-                break;
-            case 'up':
-                if(direction!=='down') {
-                    direction = 'up';}
-                break;
-            case 'left':
-                if(direction!=='right') {
-                    direction = 'left';}
-                break;
-            case 'right':
-                if(direction!=='left') {
-                    direction = 'right';}
-                break;
-        }
+    Map.prototype.theGame = function(oneDirection) {
+        setInterval(function () {
 
-        switch(direction) {
-            case 'down':
-                snakeBody.unshift([snakeBody[0][0]+1,snakeBody[0][1]]);
-                break;
-            case 'up':
-                snakeBody.unshift([snakeBody[0][0]-1,snakeBody[0][1]]);
-                break;
-            case 'left':
-                snakeBody.unshift([snakeBody[0][0],snakeBody[0][1]-1]);
-                break;
-            case 'right':
-                snakeBody.unshift([snakeBody[0][0],snakeBody[0][1]+1]);
-                break;
-        }
-
-        var snakeEnd = $("div.field[data-x='"+snakeBody[snakeBody.length-1][0]+"'][data-y='"+snakeBody[snakeBody.length-1][1]+"']");
-        var body = $("div[data-x='" + snakeBody[0][0] + "'][data-y='" + snakeBody[0][1] + "']");
-        snakeEnd.css("background-color", "aliceblue");
-        body.css("background-color", "black");
+            this.checkDirection(this.oneDirection);
 
 
-        if(snakeBody[0][0]!==bugLocation[0] || snakeBody[0][1]!==bugLocation[1]) {
-            snakeBody.pop();
-        } else {
-            do{
-                var bugx = Math.floor(mapHeight*Math.random());
-                var bugy = Math.floor(mapWidth*Math.random());
+            switch (direction) {
+                case 'down':
+                    snakeBody.unshift([snakeBody[0][0] + 1, snakeBody[0][1]]);
+                    break;
+                case 'up':
+                    snakeBody.unshift([snakeBody[0][0] - 1, snakeBody[0][1]]);
+                    break;
+                case 'left':
+                    snakeBody.unshift([snakeBody[0][0], snakeBody[0][1] - 1]);
+                    break;
+                case 'right':
+                    snakeBody.unshift([snakeBody[0][0], snakeBody[0][1] + 1]);
+                    break;
             }
-            while(snakeBody.some(function(segment){
-                return segment[0]===bugx && segment[1]===bugy;
-            }));
-            console.log(bugx, bugy);
-            bugLocation = [bugx,bugy];
-            buggy = $("div.field[data-x='"+bugLocation[0]+"'][data-y='"+bugLocation[1]+"']");
-            buggy.css("background-color", "blue");
+
+            var snakeEnd = $("div.field[data-x='" + snakeBody[snakeBody.length - 1][0] + "'][data-y='" + snakeBody[snakeBody.length - 1][1] + "']");
+            var body = $("div[data-x='" + snakeBody[0][0] + "'][data-y='" + snakeBody[0][1] + "']");
+            snakeEnd.css("background-color", "aliceblue");
+            body.css("background-color", "black");
+
+
+            if (snakeBody[0][0] !== bugLocation[0] || snakeBody[0][1] !== bugLocation[1]) {
+                snakeBody.pop();
+            } else {
+                do {
+                    var bugx = Math.floor(mapHeight * Math.random());
+                    var bugy = Math.floor(mapWidth * Math.random());
+                }
+                while (snakeBody.some(function (segment) {
+                    return segment[0] === bugx && segment[1] === bugy;
+                }));
+                console.log(bugx, bugy);
+                bugLocation = [bugx, bugy];
+                buggy = $("div.field[data-x='" + bugLocation[0] + "'][data-y='" + bugLocation[1] + "']");
+                buggy.css("background-color", "blue");
+            }
+            if (snakeBody[0][0] === mapHeight || snakeBody[0][0] === -1 || snakeBody[0][1] === mapWidth || snakeBody[0][1] === -1) {
+                clearInterval(theGame);
+                var over = $("p#over");
+                over.append('Game Over! ' + snakeBody.length)
+            }
+
+            for (var i = 1; i < snakeBody.length; i++) {
+                snakeTail[i - 1] = snakeBody[i];
+            }
+
+            if (snakeTail.some(function (snakePart) {
+                    return snakePart[0] === snakeBody[0][0] && snakePart[1] === snakeBody[0][1];
+                })) {
+                clearInterval(theGame);
+                var over = $("p#over");
+                over.append('Game Over!' + snakeBody.length)
+            }
+            ;
+
+        }, time);
+
+    }
+
+    Map.prototype.checkDirection = function(oneDirection){
+        var direction = this.direction;
+        switch (oneDirection) {
+            case 'down':
+                if (direction !== 'up') {
+                    direction = 'down';
+                }
+                break;
+            case 'up':
+                if (direction !== 'down') {
+                    direction = 'up';
+                }
+                break;
+            case 'left':
+                if (direction !== 'right') {
+                    direction = 'left';
+                }
+                break;
+            case 'right':
+                if (direction !== 'left') {
+                    direction = 'right';
+                }
+                break;
         }
-        if(snakeBody[0][0]===mapHeight || snakeBody[0][0]===-1 || snakeBody[0][1]===mapWidth || snakeBody[0][1]===-1){
-            clearInterval(theGame);
-            var over = $("p#over");
-            over.append('Game Over! ' + snakeBody.length)
-        }
+    }
 
-        for(var i = 1; i <snakeBody.length; i++) {
-            snakeTail[i-1]=snakeBody[i];
-        }
-
-        if(snakeTail.some(function(snakePart) {
-            return snakePart[0]===snakeBody[0][0] && snakePart[1]===snakeBody[0][1];
-            })) {
-            clearInterval(theGame);
-            var over = $("p#over");
-            over.append('Game Over!' + snakeBody.length)
-        };
-
-    },time);
-
+    var map1 = new Map($("div#map"));
 
     // var head = $("div[data-x='4'][data-y='4']");
     // head.css("background-color","black");
